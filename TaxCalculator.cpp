3a3c7726d17c1d95ee46c606b4a414d026c2e985
Naomi Quinones <52065567+naomiquinones@users.noreply.github.com>
2026-02-04 11:27:29 +0000

    Formats text

--- TaxCalculator.cpp
+++ TaxCalculator.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main() {
-    
+    const double TAX_RATE = 0.075;
+    double price;
+
+    cout << "Enter the price ";
+    cin >> price;
+
+    double tax = price * TAX_RATE;
+    double total = price + tax;
+
+    cout << fixed << setprecision(2) << endl;
+    cout << setw(18) << "Original Price: " << "$" << setw(8) << right << price << endl;
+    cout << setw(18) << "Sales Tax: " << "$" << setw(8) << right << tax << endl;
+    cout << setw(18) << "Total Cost: " << "$" << setw(8) << right << total << endl;
+
+    return 0;
 }
